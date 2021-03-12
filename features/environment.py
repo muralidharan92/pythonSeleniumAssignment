fbcbeba90549c77be326730f6f3b47bc26f05a77
muralidharan92 <muraleedharan005@gmail.com>
2021-03-12 21:17:31 +0530

    Added Screenshot in Allure report

---
+++ features/environment.py
@@ -0,0 +1,10 @@
+import allure
+
+
+def after_step(context, step):
+    if step.status == "failed":
+        allure.attach(context.driver.get_screenshot_as_png(), name=step.name,
+                      attachment_type=allure.attachment_type.PNG)
+    else:
+        allure.attach(context.driver.get_screenshot_as_png(), name=step.name,
+                      attachment_type=allure.attachment_type.PNG)
